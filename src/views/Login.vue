defac194293a3eeccb6fadbbd8547f56310c46af
Gengis <gengis.gutierrez.c@gmail.com>
2022-01-29 00:18:05 -0500

    Frontend first modification

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,22 +1,44 @@
 <template>
     <v-container fluid class="pa-0 fill-height">
+      
         <!-- Desktop -->
+        <span class="bg"></span>
         <v-row align="center" justify="center" class="d-none d-sm-block" >
-            <v-col md="8" lg="10" offset-md="1" sm="12" class="py-5" style="max-width:1300px;margin-left:auto;margin-right:auto"> 
-                <v-container fluid class="ma-0 pa-0 white elevation-1 mt-5">
-                <v-row align="stretch" justify="center" class="ma-0 pa-0">
-                    <v-col md="5" lg="5" sm="6" class="pa-5"> 
-                        <p class="google-font mt-5 mb-0" style="text-align: center; font-size:35px">Yupay Mikhuy</p>
-                        <br>
-                        <p class="google-font mb-5 mt-0">Cuida tu consumo :3</p>
-                        <br>
-                        <v-text-field class="ma-0 pa-0" label="Email" v-model="email" outlined></v-text-field>
-                        <v-text-field class="ma-0 pa-0" label="Password" type="password" v-model="password" outlined></v-text-field>
-                        <v-btn v-on:click="login" style="height:38px;width:150px;" :loading="loading" depressed>Login</v-btn>
-                    </v-col>
-                </v-row>
-                </v-container>
-            </v-col>
+          <v-col md="8" lg="10" offset-md="1" sm="12" class="py-5" style="max-width:1300px;margin-left:auto;margin-right:auto"> 
+            <v-container fluid class="ma-0 pa-0 elevation-1 mt-5">
+              <v-row align="center" justify="center">
+                <v-col cols="12" sm="8" md="8">
+                  <v-card>
+                    <v-row align="center" justify="center" class="ma-0 pa-0">
+                      <v-col cols="12" md="6" lg="5" class="pa-0">
+                        <v-card color="#EEC373" class="pa-4" flat>
+                          <v-img 
+                          class="pt-4 ma-10"
+                          src="https://i.imgur.com/GaaTYIJ.png"
+                          ></v-img>
+                        </v-card>
+                      </v-col>
+                      <v-col cols="12" md="6" lg="5" sm="8" class="pa-0"> 
+                        <v-card color="#FFF1BD" class="pa-4" flat>
+                          <v-img 
+                            class="pt-4 ma-10"
+                            src="https://i.imgur.com/AkWPSNz.png"
+                          ></v-img>
+                          <v-form>
+                            <v-text-field class="field" prepend-inner-icon="email" background-color="#96CEB425" label="Email" type="text"  color="#1C0D0290" v-model="email" outlined></v-text-field>
+                            <v-text-field class="field" prepend-inner-icon="lock" background-color="#96CEB425" label="Password" type="password" color="#1C0D0290" v-model="password" outlined></v-text-field>
+                          </v-form>
+                          <div class="text-center mt-3">
+                        <v-btn v-on:click="login" class="ma-12 white--text" style="height:50px; width:200px;"  rounded :loading="loading" color="#2ECC7195">Log In</v-btn>
+                          </div>
+                        </v-card>
+                      </v-col>
+                    </v-row>
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-col>
         </v-row>
         <!--Mobile-->
 
@@ -59,5 +81,27 @@ export default {
   }
 };
 </script>
+<style>
+.bg {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background: url('https://i.imgur.com/b0J7fZN.png') no-repeat center center;
+    background-size: cover;
+    background-color: rgba(0,0,0,0.8);
+    transform: scale(1);
 
+    filter:blur(6px);
+    -o-filter:blur(6px);
+    -ms-filter:blur(6px);
+    -moz-filter:blur(6px);
+    -webkit-filter:blur(6px);
+  }
+  .field {
+    margin: 2px 25px 4px 25px !important;
+    padding: 3px !important;
+  }
+</style>
 
