7d27f65e51b916608828317f708013cdbd99b880
Dante2500 <1939745+Dante2500@users.noreply.github.com>
2022-01-30 20:58:28 -0500

    avance en el diseno

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -25,8 +25,21 @@
                             src="https://i.imgur.com/AkWPSNz.png"
                           ></v-img>
                           <v-form>
-                            <v-text-field class="field" prepend-inner-icon="email" background-color="#96CEB425" label="Email" type="text"  color="#1C0D0290" v-model="email" outlined></v-text-field>
-                            <v-text-field class="field" prepend-inner-icon="lock" background-color="#96CEB425" label="Password" type="password" color="#1C0D0290" v-model="password" outlined></v-text-field>
+                            <v-text-field 
+                            class="field" 
+                            prepend-inner-icon="email" 
+                            background-color="#96CEB425" 
+                            label="Email" 
+                            type="text"  
+                            color="#1C0D0290" 
+                            v-model= "email" outlined></v-text-field>
+                            <v-text-field class="field" 
+                            prepend-inner-icon="lock" 
+                            background-color="#96CEB425" 
+                            label="Password" 
+                            type="password" 
+                            color="#1C0D0290" 
+                            v-model= "password" outlined></v-text-field>
                           </v-form>
                           <div class="text-center mt-3">
                         <v-btn v-on:click="login" class="ma-12 white--text" style="height:50px; width:200px;"  rounded :loading="loading" color="#2ECC7195">Log In</v-btn>
@@ -40,7 +53,88 @@
             </v-container>
           </v-col>
         </v-row>
+        
         <!--Mobile-->
+        <!--
+        <v-container class ="hidden-sm-and-up">
+          
+          <v-img : src="require('@/assets/backgrounds/background-desktop.png')">
+          </v-img>
+        </v-container>
+        -->
+        
+        <v-container class="hidden-sm-and-up">
+
+            <v-card >
+            <v-container fluid style="height:100vh">
+                <v-row align="center" justify="center">
+                    <v-col align="center" justify="center">
+                        <v-card class="fix2" flat>
+                          <v-img 
+                            max-width="500px"
+                            :src="require('@/assets/backgrounds/background-desktop.png')"
+                          ></v-img>
+                        <v-form>
+
+
+
+                          <v-img 
+                          width="230px"
+                          src="https://i.imgur.com/AkWPSNz.png"
+                          ></v-img>
+
+
+                        <v-card-text class="px-5 pb-0">
+                        <v-text-field
+                        
+                        class="field" 
+                            prepend-inner-icon="email" 
+                            background-color="#96CEB425" 
+                            label="Email" 
+                            type="email"  
+                            color="#1C0D0290" 
+                            v-model= "email" outlined
+                        ></v-text-field>
+
+                        <v-text-field 
+                            class="field" 
+                            prepend-inner-icon="lock" 
+                            background-color="#96CEB425" 
+                            label="Password" 
+                            type="password" 
+                            color="#1C0D0290" 
+                            v-model= "password" outlined>
+                        </v-text-field>
+                        </v-card-text>
+                    </v-form>
+                    <v-card-actions class="justify-center" >
+                        <v-btn
+                        class="ma-12 white--text"
+                        
+                        v-on:click="login" 
+                        rounded :loading="loading" 
+                        color="#3D84E1" 
+                        depressed>
+                        <span class="white--text px-8">Login</span>
+                        </v-btn>
+                        <!--
+                        <div class="text-center mt-3">
+                        <v-btn v-on:click="login" class= style="height:50px; width:200px;"  rounded :loading="loading" color="#2ECC7195">Log In</v-btn>
+                          </div>
+                        -->
+
+                    </v-card-actions>
+                    </v-card>
+                    </v-col>
+                </v-row>
+            </v-container>
+            </v-card>
+            
+        </v-container>
+        
+
+
+
 
     </v-container>
 </template>
@@ -106,5 +200,12 @@ export default {
   .fix {
     padding: 130px 10px 0px 10px !important;
   }
+  .fix2{
+    padding: 10px 10px 0px 10px !important;
+  }
+
+  .bgcel{
+    background-image: none;
+  }   
 </style>
 
