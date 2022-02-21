6f21f04c64f8d2f3a91fdf6c59ea663bd948542d
Eduardo <81939745+Dante2500@users.noreply.github.com>
2022-02-21 15:34:21 -0500

    Adding_Vcards_Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -27,6 +27,226 @@
           </v-carousel-item>
         </v-carousel>
       </v-container>
+
+      <v-container>
+        <v-row >
+          <v-col >
+
+              <v-card
+              class="mx-auto"
+              max-width="344"
+              >
+              <v-img
+                src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+                height="200px"
+              ></v-img>
+
+              <v-card-title>
+                Top western road trips {{index}}.{{item}}
+              </v-card-title>
+
+              <v-card-subtitle>
+                1,000 miles of wonder
+              </v-card-subtitle>
+
+              <v-card-actions>
+                <v-btn
+                  color="orange lighten-2"
+                  text
+                >
+                  Explore
+                </v-btn>
+
+                <v-spacer></v-spacer>
+
+                <v-btn
+                  icon
+                  
+                  @click="show = !show"
+                >
+                  <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+                </v-btn>
+              </v-card-actions>
+
+              <v-expand-transition>
+                <div v-show="show">
+                  <v-divider></v-divider>
+
+                  <v-card-text>
+                    I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
+                  </v-card-text>
+                </div>
+              </v-expand-transition>
+            </v-card>                         
+  
+          </v-col>
+
+
+                          
+
+          
+          <v-col>
+
+            <v-card
+              class="mx-auto"
+              max-width="344"
+              >
+              <v-img
+                src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+                height="200px"
+              ></v-img>
+
+              <v-card-title>
+                Top western road trips
+              </v-card-title>
+
+              <v-card-subtitle>
+                1,000 miles of wonder
+              </v-card-subtitle>
+
+              <v-card-actions>
+                <v-btn
+                  color="orange lighten-2"
+                  text
+                >
+                  Explore
+                </v-btn>
+
+                <v-spacer></v-spacer>
+
+                <v-btn
+                  icon
+                  @click="show2 = !show2"
+                >
+                  <v-icon>{{ show2 ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+                </v-btn>
+              </v-card-actions>
+
+              <v-expand-transition>
+                <div v-show="show2">
+                  <v-divider></v-divider>
+
+                  <v-card-text>
+                    I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
+                  </v-card-text>
+                </div>
+              </v-expand-transition>
+            </v-card> 
+
+
+
+          </v-col>
+          
+          
+          <v-col>
+
+              <v-card
+              class="mx-auto"
+              max-width="344"
+              >
+              <v-img
+                src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+                height="200px"
+              ></v-img>
+
+              <v-card-title>
+                Top western road trips
+              </v-card-title>
+
+              <v-card-subtitle>
+                1,000 miles of wonder
+              </v-card-subtitle>
+
+              <v-card-actions>
+                <v-btn
+                  color="orange lighten-2"
+                  text
+                >
+                  Explore
+                </v-btn>
+
+                <v-spacer></v-spacer>
+
+                <v-btn
+                  icon
+                  @click="show3 = !show3"
+                >
+                  <v-icon>{{ show3 ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+                </v-btn>
+              </v-card-actions>
+
+              <v-expand-transition>
+                <div v-show="show3">
+                  <v-divider></v-divider>
+
+                  <v-card-text>
+                    I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
+                  </v-card-text>
+                </div>
+              </v-expand-transition>
+            </v-card> 
+
+
+
+          </v-col>
+
+
+          <v-col>
+
+              <v-card
+              class="mx-auto"
+              max-width="344"
+              >
+              <v-img
+                src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+                height="200px"
+              ></v-img>
+
+              <v-card-title>
+                Top western road trips
+              </v-card-title>
+
+              <v-card-subtitle>
+                1,000 miles of wonder
+              </v-card-subtitle>
+
+              <v-card-actions>
+                <v-btn
+                  color="orange lighten-2"
+                  text
+                >
+                  Explore
+                </v-btn>
+
+                <v-spacer></v-spacer>
+
+                <v-btn
+                  icon
+                  @click="show4 = !show4"
+                >
+                  <v-icon>{{ show4 ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+                </v-btn>
+              </v-card-actions>
+
+              <v-expand-transition>
+                <div v-show="show4">
+                  <v-divider></v-divider>
+
+                  <v-card-text>
+                    I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
+                  </v-card-text>
+                </div>
+              </v-expand-transition>
+            </v-card> 
+
+          </v-col> 
+
+        </v-row>
+
+
+      </v-container>
+
+
   </v-container>
 </v-main>
    
@@ -37,6 +257,10 @@ export default {
   name: 'Home',
   data () {
       return {
+        show: false,
+        show2: false,
+        show3: false,
+        show4: false,
         colors: [
           'indigo',
           'warning',
@@ -51,9 +275,16 @@ export default {
           'Fourth',
           'Fifth',
         ],
+
       }
     },
 }
+
+  
+
+
+
+
 </script>
 <style>
   .fixed{
