a5357ff9051630a3ad779b6d5faea62d057e4fd0
Gengis <gengis.gutierrez.c@gmail.com>
2022-02-23 15:10:14 -0500

    modifying shopping list

--- src/views/ListaDeCompras.vue
+++ src/views/ListaDeCompras.vue
@@ -13,11 +13,13 @@
                 flat
                 >
                     <v-autocomplete
-                    background-color="#EEEEEE"
+                    background-color="#A1B57D50"
+                    style="border-radius: 10px"
                     filled
                     solo
                     v-model="select"    
                     flat
+                    :loading="loading"
                     :search-input.sync="search"
                     :items="items"
                     
@@ -30,19 +32,68 @@
             </v-col>
 
             <v-col cols="7">
-                <v-card>
-                    <v-row>
-                        <v-col cols="12">
-                          <p>Hola a todo el mundo</p>
-                        </v-col>
-                    </v-row>
-                </v-card>
                 <v-card
                 height="400">
+                    <v-row>
+                        <v-col cols="12">
+                          <v-col cols="4">
+                            <v-card
+                            elevation="1"
+                            class="mx-auto"
+                            >
+                              <v-img
+                              height="150"
+                              src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+                              ></v-img>
 
-                  <!--Sección encontrada en internet para listar v-card-->
-                  
-                  <!--Sección encontrada en internet para listar v-card-->
+                              <v-card-title class="ma-0" style="padding: 16px 16px 0px 16px">
+                                Cafe Badilico
+                              </v-card-title>
+
+                              <v-card-text class="ma-0" style="padding: 0px 16px 0px 16px">
+                                <div class=" text-subtitle-1">
+                                  S/. • Italian, Cafe
+                                </div>
+                              </v-card-text>
+
+                              <v-card-actions>
+                                <v-row align="center" justify="center">
+                                  <v-btn
+                                    text
+                                    color="teal accent-4"
+                                    @click="reveal = true"
+                                  >
+                                    Añadir
+                                  </v-btn>
+                                </v-row>
+                              </v-card-actions>
+                              <v-expand-transition>
+                                <v-card
+                                  v-if="reveal"
+                                  class="transition-fast-in-fast-out v-card--reveal"
+                                  style="height: 100%;"
+                                >
+                                  <v-card-text class="pb-0">
+                                    <p class="text-h4 text--primary">
+                                      Origin
+                                    </p>
+                                  </v-card-text>
+                                  <v-card-actions class="pt-0">
+                                    <v-btn
+                                      text
+                                      color="teal accent-4"
+                                      @click="reveal = false"
+                                    >
+                                      Close
+                                    </v-btn>
+                                  </v-card-actions>
+                                </v-card>
+                              </v-expand-transition>
+                            </v-card>
+
+                          </v-col>
+                        </v-col>
+                    </v-row>
                 </v-card>
             </v-col>
             <v-col cols="5">
@@ -72,21 +123,29 @@ let comidas = require('../database/db.json');
 //           .catch((err) => {
 //             console.log(err);
 //           });
-  
- console.log(comidas)
+
+let foods = [];
+for(let i = 0; i<comidas.length ;i++){
+  foods[i] = comidas[i].nombre;
+}
+
+
 
 export default {
   name: 'Lista de compras',
   data () {
     return {
+
+        //Autocomplete
+        loading: false,
         search: null,
         select: null,
         items: [],
-        food: ['Pollo', 'Chancho', 'Pescado', 'Zanahoria', 'Lechuga'],
-
+        foods,
         comidas,
-        alimentos: [],
 
+        //Show V-cards
+        reveal: false,
       }
     },
 
@@ -100,7 +159,7 @@ export default {
         this.loading = true
         // Simulated ajax query
         setTimeout(() => {
-          this.items = this.food.filter(e => {
+          this.items = this.foods.filter(e => {
             return (e || '').toLowerCase().indexOf((v || '').toLowerCase()) > -1
           })
           this.loading = false
@@ -111,5 +170,10 @@ export default {
 }
 </script>
 <style>
-    
+.v-card--reveal {
+  bottom: 0;
+  opacity: 1 !important;
+  position: absolute;
+  width: 100%;
+}
 </style>
