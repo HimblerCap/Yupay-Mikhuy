2d92cda89f85ea99f81b6b317da7269ac2e44681
Gengis <gengis.gutierrez.c@gmail.com>
2022-03-08 23:42:43 -0500

    Some changes

--- src/views/ListaDeCompras.vue
+++ src/views/ListaDeCompras.vue
@@ -150,32 +150,32 @@
                   <v-col cols="11" class="ma-0 pa-0 ">
                     <v-row justify="center" align="center">
                       <v-col cols="3" justify="center" align="center" class="pa-0 pl-1">
-                      <v-card class="pa-1" flat>
-                        <v-img
-                        max-width="70"
-                        max-hight="40"
-                        :src="item.images[0]"
-                        ></v-img>
-                      </v-card> 
-                    </v-col>
-                    <v-col cols="9" class="ma-0 pa-1">
-                      <v-row justify="center" align="center">
-                        <v-col cols="4" class="pa-0 ml-3">
+                        <v-card class="pa-1" flat>
+                          <v-img
+                          max-width="70"
+                          max-hight="40"
+                          :src="item.images[0]"
+                          ></v-img>
+                        </v-card> 
+                      </v-col>
+                      <v-col cols="9" class="ma-0 pa-1">
+                        <v-row justify="center" align="center">
+                          <v-col cols="4" class="pa-0 ml-3">
                             <h5 style="font-family: 'Lato', sans-serif;">{{item.name | capitalize}}</h5>
-                        </v-col>
-                        <v-col cols="6" class="pa-0 pl-3 ml-3">
-                          <v-text-field
-                            v-model="item.amount"
-                            label="Peso"
-                            suffix="Kg"
-                            append-outer-icon="mdi-plus"
-                            @click:append-outer="sendMessage"
-                          ></v-text-field>
-                        </v-col>
-                        <v-col cols="0" class="pa-0">
-                        </v-col>
-                      </v-row>
-                    </v-col>
+                          </v-col>
+                          <v-col cols="6" class="pa-0 pl-3 ml-3">
+                            <v-text-field
+                              v-model="item.amount"
+                              label="Peso"
+                              suffix="Kg"
+                              append-outer-icon="mdi-plus"
+                              @click:append-outer="sendMessage"
+                            ></v-text-field>
+                          </v-col>
+                          <v-col cols="0" class="pa-0">
+                          </v-col>
+                        </v-row>
+                      </v-col>
                     </v-row>
                   </v-col>  
                 </v-card>
@@ -217,7 +217,7 @@ for(let j=0; j<comidas.verduras.length;j++){
 for(let k=0; k<comidas.menestras.length;k++){
   Stews[k] = comidas.menestras[k];
 }
-foodsAll = foodsAll.concat(Fruits);
+foodsAll = foodsAll.concat(Vegetables);
 
 for(let l=0; l<foodsAll.length; l++){
   foodsAll[l].amount = ""; 
