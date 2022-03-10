4e7e7b91952280caf258463f4e9a3330d0f79592
HimblerCap <himbler.capcha.c@uni.pe>
2022-03-09 22:46:44 -0500

    Phone View of Home

---
+++ src/components/Home/HomeViewPhone.vue
@@ -0,0 +1,86 @@
+<template>
+    <v-container fluid class="ma-0 pa-0 hidden-sm-and-up" >
+        <v-row align="center" justify="center" fluid class="fill-height">
+            <v-container class="welcome-card">
+                <img src="@/assets/img/dish-welcome.png" alt="Welcome Picture" class="welcome-image">
+                <v-div class="welcome-message">
+                    Welcome
+                    <br>
+                    <br>
+                    Gengis Gutierrez!
+                </v-div>
+            </v-container>
+        </v-row>
+        <v-row align="center" fluid class="fill-height pt-10">
+            <h2 class="pt-5">Platos que puedes preparar</h2>
+            <v-card
+                class="mx-auto my-3"
+                max-width="300"
+            >
+            <v-img
+                height="150"
+                src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+            >
+            </v-img>
+
+            <v-card-title><span style="color:#FFAD60">Lomo Saltado</span></v-card-title>
+            <v-card-text>
+                Una pequeña descripción
+            </v-card-text>
+            </v-card>
+            <v-card
+                class="mx-auto my-3"
+                max-width="300"
+            >
+            <v-img
+                height="150"
+                src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+            >
+            </v-img>
+
+            <v-card-title><span style="color:#FFAD60">Lomo Saltado</span></v-card-title>
+            <v-card-text>
+                Una pequeña descripción
+            </v-card-text>
+            </v-card>
+            
+        </v-row>
+    </v-container>
+</template>
+
+<style>
+.welcome-card{
+    position: relative;
+    width: 98%;
+    height: 131px;
+    top: 25px;
+    left: 1%;
+    background: rgba(235, 193, 132, 0.794);
+    border-radius: 17px;
+}
+.welcome-image{
+    position: absolute;
+    width: 95px;
+    top: 17px;
+    height: 97px;
+    left: 25px;
+}
+.welcome-message{
+    position: absolute;
+    left: 135px;
+    top: 33.5px;
+    width: 187px;
+    height: 64px;
+    size: 20px;
+    font-weight: bolder;
+    font-size:21px;
+    line-height:16px;
+    display:flex;
+    align-items: center;
+    color: #000;
+}
+</style>
+
+
+
+
