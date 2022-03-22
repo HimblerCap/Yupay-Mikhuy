8a421e8e0de8ef509a61b1942bd9ca1e8b6eee3f
Gengis <gengis.gutierrez.c@gmail.com>
2022-03-22 17:15:51 -0500

    First change in TusAlimentos by Gengis

--- src/views/TusAlimentos.vue
+++ src/views/TusAlimentos.vue
@@ -142,7 +142,7 @@
 
 
     <v-container fluid>
-      <h2>Mis alimentos</h2>
+      <h3>Mis alimentos</h3>
     </v-container>
     
     <v-container class="ma-0 pa-0">
@@ -210,9 +210,8 @@
         </v-row>
         -->
         <v-row class="align-content-lg ma-0 pa-0"
-        v-for="(item, i) in items"
-          :key="i"
-        
+        v-for="(item, i) in myproducts"
+        :key="i"
         >
           <v-card
           width="100%"
@@ -220,24 +219,21 @@
           class="pa-2 my-2">
             <div class="d-flex flex-no-wrap justify-start align-center"  >
             <v-avatar
-             class="ma-0"
-                size="100"
-                tile>
-              <v-img :src="item.images[0]"
+            class="ma-0"
+            size="100"
+            tile>
+              <v-img :src="item.images"
               max-width="100"
               max-height="100"
-              >
-                
+              >    
               </v-img>
 
               
             </v-avatar>
             <div>
-              <v-card-title
-                  class="ma-0 pa-0 titlemob "
-                  
-                >
-                {{item.tipo}} <v-spacer></v-spacer> 
+              <v-card-title class="ma-0 pa-0 titlemob ">
+                {{item.type | capitalize}} 
+                <v-spacer></v-spacer> 
                 <v-icon v-show="item.cant_dias > 5" 
                   dark color="#FF891C" 
                   large>
@@ -246,129 +242,89 @@
                 
                 </v-card-title> <!-- item.tipo -->
 
-                <v-card-subtitle 
-                class="ma-0 pa-0 subtitlemob" v-text="item.name"></v-card-subtitle>
-                <v-card-text  class="textmob" >Inventario: 3 kg de {{item.name}}</v-card-text>
+                <v-card-subtitle class="ma-0 pa-0 subtitlemob">{{item.name | capitalize}}</v-card-subtitle>
+                <v-card-text  class="textmob" >Inventario: {{item.quantity}} kg </v-card-text>
 
-                <div align = "center">
+                <div>
                   <v-card-actions>
-                <v-btn
-                v-model= "item.statu"
+                    <v-btn
+                    v-model="item.helper"
                     small
-                depressed
-                color="#C4C4C4"
-                @click="editar(i)"
-                >
-                    MÁS INFORMACIÓN
-                </v-btn>
-                </v-card-actions>
+                    depressed
+                    color="green lighten-2"
+                    @click="showCard(i)"
+                    >
+                        MÁS INFORMACIÓN
+                    </v-btn>
+                  </v-card-actions>
 
 
                 <v-expand-transition>
-                    <v-card
-                      v-if="item.statu == false"
-                      width="100%"
-                      height="100%"
-                      class="py-2 px-1 ma-0  transition-fast-in-fast-out v-card--reveal d-flex flex-column align-center"
-                    >
+                  <v-card
+                    v-show="!item.state"
+                    width="100%"
+                    height="100%"
+                    class="py-2 px-1 ma-0  transition-fast-in-fast-out v-card--reveal d-flex flex-column align-center"
+                  >
                     <div class="ma-0 pa-0 backstyle " >
-                      
+
                       <v-card-text class="py-0 textmob ">
                         <v-row>
                           <v-col class="py-1 ">
                             Penúltimo ingreso:
                           </v-col>
-
                           <v-col class="py-1 ">
-                            {{item.penultimo_ingreso}}
+                            {{item.product_added_last}}
                           </v-col>
                         </v-row>
-
                         <v-row>
                           <v-col class="py-1 ">
                             Último ingreso:
                           </v-col>
-
                           <v-col class="py-1 ">
-                            {{item.ultimo_ingreso}}
+                            {{item.product_added_now}}
                           </v-col>
                         </v-row>
-
                         <v-row>
                           <v-col class="py-1 ">
                             Número de días:
                           </v-col>
-
                           <v-col class="py-1 ">
                             {{item.cant_dias}}
                           </v-col>
-                        </v-row>
-                        
-                        
-                      </v-card-text>
-                      
-                      
+                        </v-row>             
+                      </v-card-text>                  
+                    </div>
 
-
-                      </div>
-                      <v-card-actions class="pa-0">
-                        <v-container class="align-center justify-center">
-                        <v-btn
-                          
+                    <v-card-actions class="pa-0">
+                      <v-container class="align-center justify-center">
+                        <v-btn              
                           small
                           depressed
-                          color="#C4C4C4"
-                          @click="editar(i)"
-
-                          
-                
-                
+                          color="green lighten-2"
+                          @click="showCard(i)"
                         >
                           CERRAR
                         </v-btn>
-                        </v-container>
-                      </v-card-actions>
-                    </v-card>
-    </v-expand-transition>
+                      </v-container>
+                    </v-card-actions>
+                  </v-card>
+                </v-expand-transition>
 
                 </div>
             </div>
-
             </div>
-
           </v-card>
         </v-row>
-
-
     </v-container>
-
   </v-container>
 </v-main>
 </template>
 
 
 <script>
+import axios from 'axios'
 
-let comidas = require('../database/prueba_tottus.json');
-
-let foodsAll = [], namesAll = [], imgsAll = [];
-let Fruits = [], Vegetables = [], Stews = [];
-
-for(let i=0; i<comidas.frutas.length;i++){
-  Fruits[i] = comidas.frutas[i];
-}
-for(let j=0; j<comidas.verduras.length;j++){
-  Vegetables[j] = comidas.verduras[j];
-}
-for(let k=0; k<comidas.menestras.length;k++){
-  Stews[k] = comidas.menestras[k];
-}
-foodsAll = Fruits.concat(Vegetables);
-foodsAll = foodsAll.concat(Stews);
-
-
-
-var show =['Penultimo ingreso', 'Ultimo ingreso', 'numero de dias'];
 let items= [
         {
           name: 'Melon coquito',
@@ -403,44 +359,41 @@ let items= [
 
   export default {
     data: () => ({
-      show,
       items,
-      
+
+      index:-1,
+      reveal: false,
+
+      myproducts: null,
       amount: null,
 
-        // Extracción de datos del JSON
-        comidas,
-     
-        foodsAll,
-        imgsAll,
-        namesAll,
-
-        
-
-    }
-    
-    
-    
-    
-    
-    ),
-
-
-
+    }),
+    mounted(){
+      axios.get("https://us-central1-yupay-mikhuy.cloudfunctions.net/app/api/v1.0/users/products/MDAsyGDYliP00B1LQqjAmZSYUc02")
+      .then(response => {
+        this.myproducts = response.data  
+        for(let l=0; l<this.myproducts.length;l++){
+          this.myproducts[l].state = false
+        }
+        console.log(this.myproducts)
+      }) 
+    },
     methods: {
-
-      sendMessage () {
-        //Aqui va el método para añadir los productos a las lista de de comidas personales
-      },
       searchFilters(input, selector){
         //Aquí va el método para filtrar los alimentos
       },
-      editar: function (i){
-        this.items[i].statu =!this.items[i].statu;
-        
+      showCard(i){
+        this.myproducts[i].state =!this.myproducts[i].state;  
+        console.log(this.myproducts[i])
       }
-      
     },
+    filters: {
+          capitalize: function (value) {
+            if (!value) return ''
+            value = value.toString()
+            return value.charAt(0).toUpperCase() + value.slice(1)
+          }
+    }
   }
 </script>
 
